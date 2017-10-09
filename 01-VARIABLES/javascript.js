fb774061f3b6d706c431b53553d4ab1cdc1f07ee
user <contact@stevegravy.com>
2017-10-09 17:02:55 +0200

    exercices javascript 1 fini

---
+++ 01-VARIABLES/javascript.js
@@ -0,0 +1,32 @@
+//Exercice : Diseur de bonne aventure
+var n = 3;
+var z = "Adélaide";
+var y = "Bruxelles";
+var x = "webdeveloper";
+
+alert ('vous serez ' + x + ' et habiterez à ' + y + ' et marié à ' + z + ' avec ' + n + ' enfants!');
+
+// Calculer l'age
+var annéeCourante = 2017;
+var annéeNaissance = 1978;
+
+var age = annéeCourante - annéeNaissance;
+
+alert ('Vous avez ' + age + ' ans');
+
+
+//Calculer l'age
+var age = 39;
+var ageMax = 100;
+var denrée = 'frisks';
+var conso = 3;
+
+var reste = (ageMax - age) * (conso * 365);
+
+alert ('il vous reste ' + reste +  denrée + ' à manger avant d\' atteindre l\'age de ' + ageMax + ' ans...');
+
+
+//Priorité
+var result = (1+2) * 3 + 4 / 2;
+
+alert (result);
