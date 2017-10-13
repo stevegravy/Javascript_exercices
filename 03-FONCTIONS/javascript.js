f6cec5ec4b618f2fdb549e155523759945d49d3f
user <contact@stevegravy.com>
2017-10-13 17:00:11 +0200

    exo 4

--- 03-FONCTIONS/javascript.js
+++ 03-FONCTIONS/javascript.js
@@ -9,3 +9,11 @@ var div = function (a, b){
 var mult = function (a,b){
 	return a * b;
 }
+
+var pourcentage = function (a, b){
+	return (a / 100) * b;
+}
+
+var vitesse = function (a, b){
+	return (a / b) + "km/h";
+}
