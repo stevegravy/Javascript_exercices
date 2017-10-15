3725e0269d791e5fff7891b13a36b1de4c36d11f
Unknown <contact@stevegravy.com>
2017-10-15 13:31:04 +0200

    Début exo 5

--- 05-TABLEAUX/javascript.js
+++ 05-TABLEAUX/javascript.js
@@ -1,1 +1,8 @@
-var table = new Array();
+var tableau = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var total;
+
+for (var i = 0; i <= tableau.lenght; i++) {
+    var total = total + tableau[i];
+}
+
+console.log(total);
