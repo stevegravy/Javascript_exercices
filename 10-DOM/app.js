4c728083996ee60b7411fa323f4dbceed9c50f1b
user <contact@stevegravy.com>
2017-10-25 14:45:50 +0200

    début exercice DOM

---
+++ 10-DOM/app.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName("bg-aqua").className = "bg-olive";
+
+document.getElementById("first-paragraph").className = "aqua";
+
+document.getElementsByClassName("bg-silver").className = "bg-teal";
+
+
+element.classList.remove("bg-silver");
+
+document.getElementsByTagName()
